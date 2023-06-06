839ad6e9a989cf9b5a1f85bbe6c4c61f4287c3ae
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-06 13:24:37 -0300

    feat: user and friends association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[show index]
+  before_action :authenticate_user!, except: %i[show index about]
 end
